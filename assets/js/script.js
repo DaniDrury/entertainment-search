c9cf75cde33080246adf87156a3bf833327adc70
Meow Roar <sareacct91@gmail.com>
2023-12-08 14:07:48 -0800

    Testing done

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,8 @@ function onYouTubeIframeAPIReady() {
     },
   });
 }
-
-// function called on form submission
-async function sumbitHandler(event) {
-  event.preventDefault();
-
+// Fetch the youtube trailer and display on the iframe
+async function fetchYoutubeTrailer() {
   const userInput = searchInputEl.value.trim();
 
   const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${userInput}%201%20offical%20trailer&key=AIzaSyAUg-lxn3GSY-w58E4EFURM6w-gOrZmbOw`;
@@ -43,6 +40,74 @@ async function sumbitHandler(event) {
     alert(error.name);
     console.log(error);
   }
+}
+/* ---------------------------------------------------------- */
+
+function renderTrailer(data) {
+  ytPlayer.cueVideoById(data.videos.results[2].key);
+}
+
+function renderPoster(data) {
+  const POSTER_PATH = data.poster_path;
+
+  document.querySelector(
+    "#posterImg"
+  ).src = `https://image.tmdb.org/t/p/w780${POSTER_PATH}`;
+}
+
+async function fetchTmdbMovieDetail(movieId) {
+  // Create an url for API call
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=a3a4488d24de37de13b91ee3283244ec&append_to_response=videos,images,credits,reviews`;
+
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+
+    // Function calls
+    renderTrailer(data);
+    renderPoster(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchTmdbMovieId() {
+  // Get movie name from the user
+  const userInput = searchInputEl.value.trim();
+
+  // Change this to modal, can't use alert
+  if (!userInput) {
+    alert("please enter a valid movie name");
+  }
+  // Reset the form
+  searchInputEl.value = "";
+
+  // Create an url for an API call
+  const url = `https://api.themoviedb.org/3/search/movie?query=${userInput}&api_key=a3a4488d24de37de13b91ee3283244ec`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    // get the movidId
+    const movieId = data.results[0].id;
+    console.log(movieId);
+
+    // fetch the movidDetail
+    fetchTmdbMovieDetail(movieId);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// function called on form submission
+function sumbitHandler(event) {
+  event.preventDefault();
+
+  // fetchYoutubeTrailer();
+  fetchTmdbMovieId();
 }
 //#endregion Functions
 
