d7bbb9ec5ec8331aa0903053dbb0d0507ef584ac
Meow Roar <sareacct91@gmail.com>
2023-12-08 00:02:15 -0800

    Changed how the trailer video loads on the page. Changed loadVideoById to cueVideoByUrl to stop autoplaying. Removed onPlayerReady function - no longer needed. Reduce results from 25 to 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,10 @@ function onYouTubeIframeAPIReady() {
       playsinline: 1,
     },
     events: {
-      onReady: onPlayerReady,
+      // onReady: onPlayerReady,
       // 'onStateChange': onPlayerStateChange
     },
   });
-}
-
-// Stop autoplay of trailer when it loads
-function onPlayerReady(event) {
-  event.target.stopVideo();
 }
 
 // function called on form submission
@@ -33,14 +28,17 @@ async function sumbitHandler(event) {
 
   const userInput = searchInputEl.value.trim();
 
-  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${userInput}%201%20offical%20trailer&key=AIzaSyAUg-lxn3GSY-w58E4EFURM6w-gOrZmbOw`;
+  const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${userInput}%201%20offical%20trailer&key=AIzaSyAUg-lxn3GSY-w58E4EFURM6w-gOrZmbOw`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
 
-    ytPlayer.loadVideoById(data.items[0].id.videoId, 0);
-    ytPlayer.stopVideo();
+    // Get the trailer id
+    const videoId = data.items[0].id.videoId;
+
+    // Cue up the trailer video, but doesn't start
+    ytPlayer.cueVideoById(videoId);
   } catch (error) {
     alert(error.name);
     console.log(error);
@@ -48,4 +46,5 @@ async function sumbitHandler(event) {
 }
 //#endregion Functions
 
+// Event Listeners
 searchFormEL.addEventListener("submit", sumbitHandler);
